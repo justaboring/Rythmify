6c5e5af9c002887ee59f8f19b941ca99a8067860
justaboring <martinabdullahsyarif@gmail.com>
2026-04-11 19:44:38 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,6 @@ load_dotenv()
 
 class Config:
     DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-    SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
-    SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
-    SPOTIFY_REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI', 'http://localhost:8888/callback')
 
     FFMPEG_PATH = os.getenv('FFMPEG_PATH', 'ffmpeg')
     SKIP_VOTE_THRESHOLD = int(os.getenv('SKIP_VOTE_THRESHOLD', '50'))
